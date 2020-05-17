9ed573da5f90fed5c1a0d95d8e6337adff271969
sahil33 <sahilraza33@gmail.com>
2020-05-17 23:03:33 +0530

    Assignments 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
