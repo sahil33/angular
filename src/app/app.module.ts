f2687083bade6e7fb84c6b5889e5094dbc08e296
sahil33 <sahilraza33@gmail.com>
2020-05-18 20:59:40 +0530

    Assignment 2 and  Dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,41 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
-import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+import { LeaderService } from './services/leader.service';
+
+import 'hammerjs';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    LoginComponent
   ],
+  entryComponents: [
+    LoginComponent
+],
   imports: [
+    MatDialogModule,
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
@@ -27,9 +49,10 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+   AppRoutingModule
   ],
-  providers: [],
+  providers: [DishService,LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/dish.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Dish } from '../shared/dish';
+import { DISHES } from '../shared/dishes';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DishService {
+  
+  getDish(id: string): Dish {
+    return DISHES.filter((dish) => (dish.id === id))[0];
+  }
+  getFeaturedDish(): Dish {
+    return DISHES.filter((dish) => dish.featured)[0];
+  }
+  getDishes(): Dish[] {
+    return DISHES;
+  }
+  constructor() { }
+}
